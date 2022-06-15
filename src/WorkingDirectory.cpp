68d4cc444647b07a226a6bf8b3487416cc75ad88
Mário Cordeiro Junior <mariojuniors31@gmail.com>
2022-06-15 13:12:16 -0300

    initial commit

---
+++ src/WorkingDirectory.cpp
@@ -0,0 +1,28 @@
+#include "WorkingDirectory.hpp"
+#include <unistd.h>
+
+WorkingDirectory::WorkingDirectory()
+{
+    path = "./";
+    
+#ifdef MACOS
+    
+    // Change the default working directory to that of the XCode resource path on MacOS.
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[PATH_MAX];
+    if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
+    {
+        CFRelease(resourcesURL);
+        chdir(path);
+    }
+    
+    //TODO: why would this fail? what should we do if it does? It will result in resources not loading on mac so it is a big deal.
+
+#elif __linux__
+
+    // Change the default working directory to that of the XCode resource path on Linux.
+    path = "../src//";
+    
+#endif
+}
